196225d5ce329e39b2fc069c6d1a4d44e84a597b
Xolo, Tlatoani <negative.anaconda.agqn+code@mask.me>
2025-11-04 11:17:43 -0500

    updating based on PR comments
    version update
    
    adds support for mongo-oidc

--- src/main/java/com/dbschema/MongoJdbcDriver.java
+++ src/main/java/com/dbschema/MongoJdbcDriver.java
@@ -1,16 +1,21 @@
 package com.dbschema;
 
 import com.dbschema.mongo.DriverPropertyInfoHelper;
+import com.dbschema.mongo.MongoClientWrapper;
 import com.dbschema.mongo.MongoConnection;
+import com.dbschema.mongo.MongoService;
 import com.dbschema.mongo.mongosh.LazyShellHolder;
 import com.dbschema.mongo.mongosh.PrecalculatingShellHolder;
 import com.dbschema.mongo.mongosh.ShellHolder;
+import com.dbschema.mongo.oidc.OidcCallback;
+import com.mongodb.MongoCredential;
 import com.mongodb.mongosh.MongoShell;
 import org.graalvm.polyglot.Engine;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.sql.*;
+import java.util.Optional;
 import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.logging.Logger;
@@ -33,6 +38,7 @@ public class MongoJdbcDriver implements Driver {
   private @Nullable ExecutorService executorService;
   private @Nullable Engine sharedEngine;
   private @NotNull ShellHolder shellHolder;
+  private MongoConnection mongoConnection;
 
   static {
     try {
@@ -100,7 +106,16 @@ public class MongoJdbcDriver implements Driver {
     synchronized (this) {
       ShellHolder shellHolder = this.shellHolder;
       this.shellHolder = createShellHolder();
-      return new MongoConnection(url, info, username, password, fetchDocumentsForMeta, shellHolder);
+      MongoCredential.OidcCallbackContext existingResult = Optional.ofNullable(this.mongoConnection)
+              .map(MongoConnection::getService)
+              .map(MongoService::getClient)
+              .map(MongoClientWrapper::getOidcCallback)
+              .map(OidcCallback::getCallbackContext)
+              .orElse(null);
+
+      this.mongoConnection = new MongoConnection(url, info, username, password, fetchDocumentsForMeta, shellHolder, existingResult);
+
+      return this.mongoConnection;
     }
   }
 
